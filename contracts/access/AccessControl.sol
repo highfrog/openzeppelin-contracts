76bc7abd0079b3cae5ae9b1ec9159d5ed16a0caf
highfrog <40280999+highfrog@users.noreply.github.com>
2022-03-10 21:24:25 -0500

    Update AccessControl.sol

--- contracts/access/AccessControl.sol
+++ contracts/access/AccessControl.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.5.0) (access/AccessControl.sol)
 
-pragma solidity ^0.8.0;
+pragma solidity >=0.4.22;
 
 import "./IAccessControl.sol";
 import "../utils/Context.sol";
